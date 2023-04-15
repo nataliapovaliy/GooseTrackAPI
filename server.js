49fb982c3062b6c93a714a8d5b3c8092995887af
VladislavSurkov <fvhabjrega@gmail.com>
2023-04-15 15:07:19 +0300

    done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() =>
-    app.listen(3000, () => console.log("Database connection successful"))
+    app.listen(process.env.PORT, () => console.log("Database connection successful"))
   )
   .catch((error) => {
     console.log(error.message);
